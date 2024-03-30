704fd3e5195a56c50cff55e409bd705d9e5f8e13
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-03-30 12:32:05 +0800

    Display data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { format } from "date-fns";
 let location = "baao";
 let date;
 let condition;
+let conIcon;
 let feelsLike;
 let humidity;
 let sunrise;
@@ -10,11 +11,15 @@ let sunset;
 let chanceOfRain;
 let temp;
 let windSpeed;
+let uvIndex;
+let current;
+let weekly;
 
-const dailyForecast = (day, temperature, weatherCondition) => ({
+const dailyForecast = (day, temperature, weatherCondition, icon) => ({
   day,
   temperature,
   weatherCondition,
+  icon,
 });
 
 function extractCurrentData(forecast) {
@@ -36,7 +41,10 @@ function extractCurrentData(forecast) {
   date = `${dateTime[0]} ${time}`;
   condition = latestForecast.day.condition.text;
   humidity = `${current.humidity}%`;
+
   chanceOfRain = `${latestForecast.day.daily_chance_of_rain}%`;
+  uvIndex = current.uv;
+  conIcon = latestForecast.day.condition.icon;
   sunrise = latestForecast.astro.sunrise;
   sunset = latestForecast.astro.sunset;
   temp = [tempC, tempF];
@@ -47,27 +55,16 @@ function extractCurrentData(forecast) {
     location,
     date,
     condition,
+    conIcon,
     chanceOfRain,
     humidity,
+    uvIndex,
     temp,
     feelsLike,
     windSpeed,
     sunrise,
     sunset,
   ];
-
-  /* console.log(
-    location,
-    date,
-    condition,
-    chanceOfRain,
-    humidity,
-    temp,
-    feelsLike,
-    windSpeed,
-    sunrise,
-    sunset
-  ); */
 
   return data;
 }
@@ -82,12 +79,11 @@ function extractWeeklyData(forecast) {
     const tempF = `${forecastday[i].day.avgtemp_f}°F`;
     const dailyTemp = [tempC, tempF];
     const dailyCondition = forecastday[i].day.condition.text;
-    const daily = dailyForecast(day, dailyTemp, dailyCondition);
+    const dailyIcon = forecastday[i].day.condition.icon;
+    const daily = dailyForecast(day, dailyTemp, dailyCondition, dailyIcon);
 
     weeklyForecast.push(daily);
   }
-
-  /* console.log(weeklyForecast); */
 
   return weeklyForecast;
 }
@@ -111,9 +107,73 @@ async function getForecast(loc) {
   }
 }
 
+function displayData(current, weekly) {
+  const main = document.querySelector("main");
+  const location = document.querySelector(".location");
+  const date = document.querySelector(".date");
+  const conIcon = document.querySelector(".conIcon");
+  const condition = document.querySelector(".condition");
+  const temp = document.querySelector(".temp");
+  const feelsLike = document.querySelector(".feelsLike");
+  const uvIndex = document.querySelector(".uvIndex");
+  const humidity = document.querySelector(".humidity");
+  const chanceOfRain = document.querySelector(".chanceOfRain");
+  const windSpeed = document.querySelector(".windSpeed");
+  const sunrise = document.querySelector(".sunrise");
+  const sunset = document.querySelector(".sunset");
+  const weeklyForecast = document.querySelectorAll(".weekly");
+
+  location.textContent = current[0];
+  date.textContent = current[1];
+  condition.textContent = current[2];
+  uvIndex.textContent = current[6];
+  humidity.textContent = current[5];
+  chanceOfRain.textContent = current[4];
+  sunrise.textContent = current[10];
+  sunset.textContent = current[11];
+
+  conIcon.src = current[3];
+
+  if (main.classList.contains("celsius")) {
+    temp.textContent = current[7][0];
+    feelsLike.textContent = current[8][0];
+    windSpeed.textContent = current[9][0];
+  } else {
+    temp.textContent = current[7][1];
+    feelsLike.textContent = current[8][1];
+    windSpeed.textContent = current[9][1];
+  }
+
+  let count = 0;
+
+  weeklyForecast.forEach((forecast) => {
+    const day = document.createElement("span");
+    const temp = document.createElement("span");
+    const icon = document.createElement("img");
+    const condition = document.createElement("span");
+
+    while (forecast.lastElementChild)
+      forecast.removeChild(forecast.lastElementChild);
+
+    day.textContent = weekly[count].day;
+    condition.textContent = weekly[count].weatherCondition;
+
+    icon.src = weekly[count].icon;
+
+    if (main.classList.contains("celsius"))
+      temp.textContent = weekly[count].temperature[0];
+    else temp.textContent = weekly[count].temperature[1];
+
+    forecast.append(day, temp, icon, condition);
+
+    count += 1;
+  });
+}
+
 getForecast(location).then((forecast) => {
-  const current = extractCurrentData(forecast);
-  const weekly = extractWeeklyData(forecast);
+  current = extractCurrentData(forecast);
+  weekly = extractWeeklyData(forecast);
+  displayData(current, weekly);
   console.log(current, weekly);
 });
 
@@ -129,6 +189,7 @@ searchBtn.addEventListener("click", (e) => {
   getForecast(location).then((forecast) => {
     const current = extractCurrentData(forecast);
     const weekly = extractWeeklyData(forecast);
+    displayData(current, weekly);
     console.log(current, weekly);
   });
 });
